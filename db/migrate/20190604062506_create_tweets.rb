929db2e4c09e6a14f5ba3f2bc3833a4c0efc390a
emiel hagen <emiel.hagen@gmail.com>
2019-06-04 17:35:06 +1000

    new rails project

---
+++ db/migrate/20190604062506_create_tweets.rb
@@ -0,0 +1,11 @@
+class CreateTweets < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tweets do |t|
+      t.text :body
+      t.string :sentiment
+      t.decimal :score
+
+      t.timestamps
+    end
+  end
+end
